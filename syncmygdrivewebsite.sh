df7a7f6936d8761c98c5339ff21705b862c2278b
Sri Vari Engg - Coimbatore <83562972+srivari2021@users.noreply.github.com>
2022-06-22 15:21:15 +0530

    Add files via upload

---
+++ syncmygdrivewebsite.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#I use this to download web content from Google Drive
+
+gdrive sync download 1byRFAnVEZDaia4hcolFaXY2bq86UDyV1 /var/www/html
+#gdrive sync upload /var/www/html 1byRFAnVEZDaia4hcolFaXY2bq86UDyV1
+
