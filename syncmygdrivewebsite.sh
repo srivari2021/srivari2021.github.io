5a4291110e345a16e37ea346844a25ffffb16576
Sankar Ramasamy <rsankarpollachi@gmail.com>
2022-06-22 15:32:56 +0530

    restored from srivariengineering.com

--- syncmygdrivewebsite.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#I use this to download web content from Google Drive
-
-gdrive sync download 1byRFAnVEZDaia4hcolFaXY2bq86UDyV1 /var/www/html
-#gdrive sync upload /var/www/html 1byRFAnVEZDaia4hcolFaXY2bq86UDyV1
-
